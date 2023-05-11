6253b07afb21a689a786dc5032d43ba7fcb3e7a6
princesingh04 <111722098+princesingh04@users.noreply.github.com>
2023-05-11 18:19:58 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+let a;
+let b = prompt("Enter the number": );
+if(b==a)
+  console.log("Is equal");
+else if (a>b){
+  console.log("The number is smaller");
+}
+else{
+  console.log("The number is greater");
+}
